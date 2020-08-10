cedf7610170d7be31654418a0672cdb8e7e1cdf4
Nick <ptnguyen1901@gmail.com>
2020-08-09 20:05:02 -0400

    Initial Commit

---
+++ ParsingJSON/ParsingJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParsingJSONApp.swift
+//  ParsingJSON
+//
+//  Created by Nick Nguyen on 8/9/20.
+//
+
+import SwiftUI
+
+@main
+struct ParsingJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
